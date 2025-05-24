576aba9fb3997555b088e5df96abdd1971e2f9e5
Trev-D-Dev <dunn.trevor@pm.me>
2025-05-24 16:12:20 -0400

    Created player, is drawn on screen

--- main.py
+++ main.py
@@ -6,18 +6,23 @@ import pygame
 # imports all the constant values
 from constants import *
 
+from player import *
+
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+   
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill((0, 0, 0)) # sets screen to black using tuple of rgb
+        player.draw(screen)
         pygame.display.flip()
         milliseconds = clock.tick(60)
         dt = milliseconds / 1000 # converts milliseconds to seconds
