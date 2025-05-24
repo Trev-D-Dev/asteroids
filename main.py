b585d2f3edb05640bf8d07fb62ebd34220556c82
Trev-D-Dev <dunn.trevor@pm.me>
2025-05-24 16:29:33 -0400

    Added containers for items to be update or drawn

--- main.py
+++ main.py
@@ -13,17 +13,27 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-   
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill((0, 0, 0)) # sets screen to black using tuple of rgb
-        player.update(dt)
-        player.draw(screen)
+
+        for item in updatable:
+            item.update(dt)
+
+        for item in drawable:
+            item.draw(screen)
+
         pygame.display.flip()
         milliseconds = clock.tick(60)
         dt = milliseconds / 1000 # converts milliseconds to seconds
